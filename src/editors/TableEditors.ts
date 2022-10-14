f20cce7dd4905e858b60fa0eb70ce70ec424282f
maximilianweidenauer <66732863+maximilianweidenauer@users.noreply.github.com>
2022-10-14 11:52:40 +0200

    added tree and toolbar to table, and 2 variables

--- src/editors/TableEditors.ts
+++ src/editors/TableEditors.ts
@@ -3,3 +3,23 @@ import { EditorItem } from "./management/EditorCreator";
 const docStyle = window.getComputedStyle(document.documentElement);
 
 export const tableEditors: Map<string, EditorItem[]> = new Map<string, EditorItem[]>()
+.set("Table", [
+    {
+        variable: "--table-border",
+        label: "Table Border",
+        type: "text",
+        cssType: "scheme",
+        value: docStyle.getPropertyValue('--table-border'),
+        usage: new Map<string, string[]>().set(".rc-table", ["border: var(--table-border);"])
+    },
+    {
+        variable: "--table-header-background",
+        label: "Header Background",
+        type: "color",
+        cssType: "scheme",
+        value: docStyle.getPropertyValue('--table-header-background'),
+        usage: new Map<string, string[]>()
+        .set(".p-autocomplete-panel.dropdown-table .p-autocomplete-item-group > *", ["background: var(--table-header-background);"])
+        .set(".rc-table .p-datatable-thead > tr > th", ["background: var(--table-header-background);"])
+    },
+])
--- src/previews/tabletree/TableTree.tsx
+++ src/previews/tabletree/TableTree.tsx
@@ -1,40 +1,183 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useState, CSSProperties } from 'react';
 import { DataTable } from 'primereact/datatable';
-import { Tree } from 'primereact/tree';
+import { Tree, TreeSelectionKeys } from 'primereact/tree';
+import { Button } from 'primereact/button';
 import { Column } from 'primereact/column';
 import * as _ from 'underscore'
+import { concatClassnames } from '../../util/ConcatClassNames';
+import tinycolor from 'tinycolor2';
+
+const tableItems = [
+    {
+        firstName: "Max",
+        lastName: "Mustermann",
+        age: 30,
+        city: "Vienna"
+    },
+    {
+        firstName: "John",
+        lastName: "Doe",
+        age: 43,
+        city: "London"
+    },
+    {
+        firstName: "Brandon",
+        lastName: "Mills",
+        age: 47,
+        city: "Austin"
+    }
+];
+
+const treeItems = [
+    {
+        key: "0",
+        label: "1",
+        icon: "pi pi-fw pi-folder-open",
+        children: [
+            {
+                key: "0-0",
+                label: "1.1",
+                icon: "pi pi-fw pi-code",
+                children: [
+                    {
+                        key: "0-0-0",
+                        label: "1.1.1",
+                        icon: "pi pi-fw pi-star-fill",
+                    },
+                    {
+                        key: "0-0-1",
+                        label: "1.1.2",
+                        icon: "pi pi-fw pi-star-fill",
+                    }
+                ]
+            },
+            {
+                key: "0-1",
+                label: "1.2",
+                icon: "pi pi-fw pi-code",
+                children: [
+                    {
+                        key: "0-1-0",
+                        label: "1.2.1",
+                        icon: "pi pi-fw pi-star-fill",
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        key: "1",
+        label: "2",
+        icon: "pi pi-fw pi-folder-open",
+        children: [
+            {
+                key: "1-0",
+                label: "2.1",
+                icon: "pi pi-fw pi-code",
+            },
+            {
+                key: "1-1",
+                label: "2.2",
+                icon: "pi pi-fw pi-code",
+            },
+            {
+                key: "1-2",
+                label: "2.3",
+                icon: "pi pi-fw pi-code",
+            }
+        ]
+    },
+    {
+        key: "2",
+        label: "3",
+        icon: "pi pi-fw pi-folder-open",
+        children: [
+            {
+                key: "2-0",
+                icon: "pi pi-fw pi-code",
+                label: "3.1",
+                children: [
+                    {
+                        key: "2-0-0",
+                        label: "3.1.1",
+                        icon: "pi pi-fw pi-star-fill",
+                    },
+                    {
+                        key: "2-0-1",
+                        label: "3.1.2",
+                        icon: "pi pi-fw pi-star-fill",
+                    }
+                ]
+            },
+            {
+                key: "2-1",
+                label: "3.2",
+                icon: "pi pi-fw pi-code",
+                children: [
+                    {
+                        key: "2-1-0",
+                        label: "3.2.1",
+                        icon: "pi pi-fw pi-star-fill",
+                    },
+                    {
+                        key: "2-1-1",
+                        label: "3.2.2",
+                        icon: "pi pi-fw pi-star-fill",
+                    }
+                ]
+            }]
+    }
+]
 
 const TableTree:FC = () => {
-
-    const items = [
-        {
-            firstName: "Max",
-            lastName: "Mustermann",
-            age: 30,
-            city: "Vienna"
-        },
-        {
-            firstName: "John",
-            lastName: "Doe",
-            age: 43,
-            city: "London"
-        },
-        {
-            firstName: "Brandon",
-            lastName: "Mills",
-            age: 47,
-            city: "Austin"
-        }
-    ];
-
-    const [selectedTableItem, setSelectedTableItem] = useState<{value: any, field: string, rowData: any, rowIndex: number, cellIndex: number}>({ cellIndex: 0, field: "firstName", rowData: items[0], rowIndex: 0, value: "Max" });
+    const [selectedTableItem, setSelectedTableItem] = useState<{value: any, field: string, rowData: any, rowIndex: number, cellIndex: number}>({ cellIndex: 0, field: "firstName", rowData: tableItems[0], rowIndex: 0, value: "Max" });
+    const [selectedTreeItem, setSelectedTreeItem] = useState<TreeSelectionKeys>(null);
+    const btnBgd = window.getComputedStyle(document.documentElement).getPropertyValue('--primary-color');
 
     return (
         <div className="preview-container">
-            <div>
+            <div className='comp-container table-container'>
+                <div className='rc-toolbar navbar-west'>
+                    <Button
+                        className={concatClassnames(
+                            "rc-button",
+                            tinycolor(btnBgd.toString()).isDark() ? "bright-button" : "dark-button",
+                            "mouse-border",
+                            "gap-right",
+                            "rc-toolbar-button",
+                            "no-focus-rect"
+                        )}
+                        icon="pi pi-plus-circle"
+                        style={{ "--background": btnBgd, "--hoverBackground": tinycolor(btnBgd.toString()).darken(5).toString(), '--iconTextGap': '4px' } as CSSProperties}
+                    />
+                    <Button
+                        className={concatClassnames(
+                            "rc-button",
+                            tinycolor(btnBgd.toString()).isDark() ? "bright-button" : "dark-button",
+                            "mouse-border",
+                            "gap-right",
+                            "rc-toolbar-button",
+                            "no-focus-rect"
+                        )}
+                        icon="pi pi-times-circle"
+                        style={{ "--background": btnBgd, "--hoverBackground": tinycolor(btnBgd.toString()).darken(5).toString(), '--iconTextGap': '4px' } as CSSProperties}
+                    />
+                    <Button
+                        className={concatClassnames(
+                            "rc-button",
+                            tinycolor(btnBgd.toString()).isDark() ? "bright-button" : "dark-button",
+                            "mouse-border",
+                            "gap-right",
+                            "rc-toolbar-button",
+                            "no-focus-rect"
+                        )}
+                        icon="pi pi-pencil"
+                        style={{ "--background": btnBgd, "--hoverBackground": tinycolor(btnBgd.toString()).darken(5).toString(), '--iconTextGap': '4px' } as CSSProperties}
+                    />
+                </div>
                 <DataTable
                     className='rc-table'
-                    value={items}
+                    value={tableItems}
                     selection={selectedTableItem}
                     selectionMode="single"
                     cellSelection
@@ -52,6 +195,14 @@ const TableTree:FC = () => {
                         <Column field="age" header="Age"></Column>
                         <Column field="city" header="City"></Column>
                     </DataTable>
+            </div>
+            <div className='comp-container tree-container'>
+                <Tree 
+                 value={treeItems}
+                 className="rc-tree"
+                 selectionMode='single'
+                 selectionKeys={selectedTreeItem}
+                 onSelectionChange={event => setSelectedTreeItem(event.value)} />
             </div>
         </div>
     )
