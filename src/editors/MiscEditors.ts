22067e643d8c6d63ac6dfb4075e821c8318d4bfa
maximilianweidenauer <66732863+maximilianweidenauer@users.noreply.github.com>
2022-10-25 15:59:48 +0200

    Create MiscEditors.ts

---
+++ src/editors/MiscEditors.ts
@@ -0,0 +1,15 @@
+import { EditorItem } from "./management/EditorCreator";
+
+const docStyle = window.getComputedStyle(document.documentElement);
+
+export const miscEditors: Map<string, EditorItem[]> = new Map<string, EditorItem[]>()
+.set("Loading-Topbar", [
+    {
+        variable: "--topbar-colors",
+        label: "Color",
+        type: "text",
+        cssType: "scheme",
+        value: docStyle.getPropertyValue('--topbar-colors'),
+        usage: new Map<string, string[]>()
+    }
+])
