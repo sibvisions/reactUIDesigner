77438a920d06c548cd2dc5a7b9ab1f95fea7330e
maximilianweidenauer <66732863+maximilianweidenauer@users.noreply.github.com>
2022-10-25 15:44:51 +0200

    Update ReactUIDesigner.tsx

--- src/ReactUIDesigner.tsx
+++ src/ReactUIDesigner.tsx
@@ -24,6 +24,7 @@ import { sendRequest } from './RequestService';
 import { Toast } from 'primereact/toast';
 import { generateCSS } from './util/GenerateCSS';
 import { Accordion, AccordionTab } from 'primereact/accordion';
+import './index.scss';
 
 interface IReactUIDesigner {
   isCorporation: boolean
